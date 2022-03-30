6b761afdd179a385cbecf2f7948a398fdc04fee4
asadbek <asadbekgit@gmail.com>
2022-03-29 20:23:42 -1200

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let salom = prompt("Ismingizni kiriting:")
+console.log(`Mening Ismim: ${salom}`)
+
+let salom1 = prompt("Yoshingizni kiriting:")
+console.log(`Yoshim: ${salom1} da`)
+
+let salom3 = prompt("Misolni yeching 15 + 5 = ?:")
+console.log(`1. Misoldagi jovobingiz: 15 + 5 = ${salom3}`)
+
+let salom4 = prompt("Misolni yeching 20 - 2 = ?:")
+console.log(`2. Misoldagi jovobingiz: 20 + 2 = ${salom4}`)
+
+let salom5 = prompt("Misolni yeching 50 / 5 = ?:")
+console.log(`3. Misoldagi jovobingiz: 50 / 5 = ${salom5}`)
+
+let salom6 = prompt("Misolni yeching 10 % 2 = ?:")
+console.log(`4. Misoldagi jovobingiz: 10 % 2 = ${salom6}`)
+
+alert("jovobingizni bilish uchun 'console'ni oching")
+
